f0d779a97e6d338f82fa0c6fb15647842e30dc3a
acremer01 <allisoncremer01@gmail.com>
2020-03-31 21:22:48 -0600

    final touches

--- Step 5/step5_simulationperiod.py
+++ Step 5/step5_simulationperiod.py
@@ -7,17 +7,14 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-#import scipy.signal as sig
+import scipy.signal as sig
 
 # GLOBAL VARIABLES
 gravity = 9.8
-damping = .9995
+damping = .99993
 
 # CUSTOM FUNCTIONS
 def update_system(acc,pos,vel,time1,time2,armlength):
-    # takes in acceleration, position, velocity, time, and length parameters
-    # and updates them according to the time interval
-    # returns the new pos, vel, and acc
     # position and velocity update below
     dt = time2-time1
     velNext = (vel+acc*dt)*damping
@@ -26,7 +23,6 @@ def update_system(acc,pos,vel,time1,time2,armlength):
     return posNext,velNext,accNext
 
 def print_system(time,pos,vel,acc):
-    # prints the system details
     print("TIME:         ", time)
     print("POSITION:     ", pos)
     print("VELOCITY:     ", vel)
@@ -36,34 +32,32 @@ def create_graph(time, pos, vel, acc):
     plt.figure(3, figsize=(15,10))
     # position subplot
     plt.subplot(3,1,1)
-    plt.plot(time, pos, 'ro--') 
+    plt.plot(time, pos, '#9467bd') 
     plt.xlabel('Time (seconds)')
     plt.ylabel('Position (m)')
     plt.title('Position vs Time')
-    plt.xlim((0, 10)) 
+    plt.xlim((0, 35)) 
     plt.grid()
     # velocity subplot
     plt.subplot(3,1,2)
-    plt.plot(time, vel, 'ro--') 
+    plt.plot(time, vel, '#17becf') 
     plt.xlabel('Time (seconds)')
     plt.ylabel('Velocity (m/s)')
     plt.title('Velocity vs Time')
-    plt.xlim((0, 10)) # set x range to -1 to 8
+    plt.xlim((0, 35)) # set x range to -1 to 8
     plt.grid()
     # acceleration subplot
     plt.subplot(3,1,3)
-    plt.plot(time, acc, 'ro--') 
+    plt.plot(time, acc, '#2ca02c') 
     plt.xlabel('Time (seconds)')
     plt.ylabel('Acceleration (m/s^2)')
     plt.title('Acceleration vs Time')
-    plt.xlim((0, 10)) # set x range to -1 to 8
+    plt.xlim((0, 35)) # set x range to -1 to 8
     plt.grid()
     plt.tight_layout()
     plt.show()
     
 def find_peaks(arr):
-    # find_peaks find the peaks of a certain array in order to find the period
-    # of each pendulum length
     time = np.array(arr[0])
     y = np.array(arr[1])
     y_filt = sig.medfilt(y)
@@ -82,6 +76,27 @@ def create_peak_graph(time, y, y_filt):
     plt.tight_layout()
     plt.show()
 
+def find_avg_period(atime, atheta):
+    time = np.array(atime)
+    theta = np.array(atheta)
+    theta=sig.medfilt(theta)
+    peaks=sig.find_peaks(theta)
+    periodtotal=0
+    count=0
+    for i in range(len(peaks[0])-2):
+        index1=int(peaks[0][i])
+        time1=time[index1]
+        index2=int(peaks[0][i+1])
+        time2=time[index2]
+        period=time2-time1
+        while period<1 and i<len(peaks[0])-2:
+            i+=1
+            time2=time[peaks[0][i+1]]
+            period=time2-time1
+        periodtotal+=period
+        count+=1
+    averageperiod=periodtotal/count
+    return averageperiod
 
 #MAIN
 # initial conditions
@@ -91,7 +106,7 @@ for armlength in lengths:
     pos = [(math.pi*7/6)]
     vel = [0]
     acc = [0]
-    time = np.linspace(0,10,10001)
+    time = np.linspace(0,35,35001)
     print_system(time[0],pos[0],vel[0],acc[0])
     i = 1
     while i < len(time):
@@ -103,5 +118,5 @@ for armlength in lengths:
         #print_system(time[i],pos[i],vel[i], acc[i])
         i += 1
     create_graph(time, pos, vel, acc)
-    #find_peaks()
+    print('The simulated period for', armlength, 'm is', find_avg_period(time, pos), 's')
 
